4d9f8dd2df8be9850c7ed907715d77f3974c6a6e
Federico <steidlmartinez@gmail.com>
2025-07-11 13:12:42 +0200

    simple debug output to console

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <Windows.h>
-#include <winuser.h>
+#include <stdio.h>
+
+#undef UNICODE
 
 LRESULT CALLBACK mainWindowCallback
 (
@@ -14,33 +16,50 @@ LRESULT CALLBACK mainWindowCallback
     {
         case WM_SIZE:
         {
-            //TODO:
+            //TODO: handle
+            printf("WM_SIZE");
             break;
         }
         case WM_DESTROY:
         {
-            //TODO:
+            //TODO: handle
+            printf("WM_DESTROY");
             break;
         }
         case WM_CLOSE:
         {
-            //TODO:
+            //TODO: handle
+            printf("WM_CLOSE");
             break;
         }
         case WM_ACTIVATEAPP:
         {
-            //TODO:
+            //TODO: handle
+            printf("WM_ACTIVATEAPP");
             break;
         }
         default:
         {
-            result = DefWindowProcA(window, message, wParam, lParam);
+            result = DefWindowProc(window, message, wParam, lParam);
             break;
         }
     }
 
     return result;
 }
+
+#ifdef DEBUG
+int main()
+{
+    STARTUPINFO startupInfo;
+    GetStartupInfo(&startupInfo);
+
+    WinMain(0, 0,
+            GetCommandLine(),
+            0);
+}
+
+#endif
 
 int CALLBACK WinMain
 (
@@ -56,16 +75,42 @@ int CALLBACK WinMain
     wc.hInstance = instance;
     wc.lpszClassName = "CPongClass";
 
-    if(!RegisterClassA(&wc))
+    if(!RegisterClass(&wc))
     {
-        return -99;
+        printf("unable to register window class.");
+        return 99;
     };
 
-    int x = CW_USEDEFAULT;
-    int y = CW_USEDEFAULT;
-    int width = CW_USEDEFAULT;
+    int x      = CW_USEDEFAULT;
+    int y      = CW_USEDEFAULT;
+    int width  = CW_USEDEFAULT;
     int height = CW_USEDEFAULT;
 
-    CreateWindowExA(0, wc.lpszClassName, "CPong", wc.style,
-                    x, y, width, height, 0, 0, instance, 0);
+    HWND windowHandle = CreateWindowEx(0,
+                                       wc.lpszClassName,
+                                       "CPong",
+                                       wc.style,
+                                       x, y, width, height,
+                                       0, 0, instance, 0);
+    if(!windowHandle)
+    {
+        printf("unable to obtain window handle.");
+        return 87;
+    }
+
+    MSG message;
+    for(;;)
+    {
+        BOOL msgResult = GetMessage(&message, 0, 0, 0);
+
+        if(msgResult <= 0)
+        {
+            break;
+        }
+
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+    return 0;
 }
