b0cdeac1402ea7bece0a1a8826f87e4d5b460540
Federico <steidlmartinez@gmail.com>
2025-08-02 11:30:20 +0200

    add normalized coordinates

--- include/main.h
+++ include/main.h
@@ -3,3 +3,18 @@
 #define persistent  static
 #define global      static
 #define internal    static
+
+#include <stdint.h>
+
+typedef struct
+{
+    float x;
+    float y;
+}Coordinate;
+
+typedef struct
+{
+    Coordinate coord;
+    float      h_vel;
+    float      v_vel;
+}Ball;
