92ff9e9483b6c8c747d0e2154a254d68a453ab00
Federico <steidlmartinez@gmail.com>
2025-08-08 17:59:05 +0200

    ignore unused parameters msvc flag

--- premake5.lua
+++ premake5.lua
@@ -15,7 +15,7 @@ workspace("CPong")
         objdir("obj/Win64_%{cfg.buildcfg}")
         includedirs({ "./include/", })
         files({ "./src/*", "./include/*" })
-        buildoptions{"/wd4068"}
+        buildoptions{"/wd4068", "/wd4100"}
 
 filter("configurations:Debug")
     kind("ConsoleApp")
